393321e6f18fb3db30a53b76e67651c43398cf1a
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-01-23 19:27:10 -0600

    changing only a flag on useradd, doesnt change much

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -3,11 +3,12 @@
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
-
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" --create-home madmex_user && echo 'madmex_user:qwerty' | chpasswd
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m  madmex_user && echo 'madmex_user:qwerty' | chpasswd
+useradd --shell /bin/bash -u $USER_ID -o -c "" postgres 
+echo "madmex_user ALL=(ALL:ALL) NOPASSWD:ALL" | (EDITOR="tee -a" visudo)
 export HOME=/home/madmex_user
 chown madmex_user:madmex_user /home/madmex_user/
 chmod 750 /home/madmex_user
