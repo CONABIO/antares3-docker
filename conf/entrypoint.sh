c19616fa75ee9dc5c53da44241ab0e6ac21a65d6
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-01-24 10:24:31 -0600

    dont remember what was the change...

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -6,7 +6,7 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m  madmex_user && echo 'madmex_user:qwerty' | chpasswd
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m madmex_user && echo 'madmex_user:qwerty' | chpasswd
 useradd --shell /bin/bash -u $USER_ID -o -c "" postgres 
 echo "madmex_user ALL=(ALL:ALL) NOPASSWD:ALL" | (EDITOR="tee -a" visudo)
 export HOME=/home/madmex_user
