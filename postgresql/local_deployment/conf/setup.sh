61b1c6f5a9e07eb0328e381a3ddae382be113118
Erick <palmoreck@gmail.com>
2019-08-23 15:34:28 -0500

    adding local deployment for postgresql

---
+++ postgresql/local_deployment/conf/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+nombre_base_de_datos=antares_datacube
+sudo apt-get update
+sudo service ssh restart
+cp /etc/skel/.bashrc /home/postgres/.
+cp /etc/skel/.profile /home/postgres/.
+sudo apt-get install -y \
+            postgresql \
+            postgresql-client \
+            postgresql-10-postgis-2.4
+
+sudo /etc/init.d/postgresql start
+sudo echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/10/main/pg_hba.conf
+sudo echo "local all all md5" >> /etc/postgresql/10/main/pg_hba.conf
+sudo echo "listen_addresses='*'" >> /etc/postgresql/10/main/postgresql.conf
+psql --command "ALTER USER postgres WITH PASSWORD 'postgres';"
+psql --command "CREATE EXTENSION postgis;"
+sudo /etc/init.d/postgresql restart
+echo "export LC_ALL=C.UTF-8" >> ~/.profile
+echo "export LANG=C.UTF-8" >> ~/.profile
+createdb $nombre_base_de_datos
